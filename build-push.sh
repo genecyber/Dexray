0e5541aede12d74a28e5e71be975cd32fde217b9
Shannon Code <genecyber@gmail.com>
2020-03-11 14:04:14 -0400

    deploy correctly

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,5 @@
-docker build . -t us.gcr.io/multichain-prod/dexray2:v1 $1
-gcloud docker -- push us.gcr.io/multichain-prod/dexray2
+docker build . -t us.gcr.io/multichain-prod/dexray3:v1 $1
+gcloud docker -- push us.gcr.io/multichain-prod/dexray3
 cd k8s/
 kubectl apply -f app.yaml
 cd ..
